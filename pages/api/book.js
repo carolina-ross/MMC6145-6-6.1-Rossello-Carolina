2ebb40bec501d5c1e4a0c4111bffdd344f18b4a1
Carolina Rossello <c.rossello@ufl.edu>
2023-04-12 00:40:38 -0400

    last tests ugh

--- pages/api/book.js
+++ pages/api/book.js
@@ -10,17 +10,43 @@ export default withIronSessionApiRoute(
     // TODO: Respond with 404 for all other requests
     // User info can be accessed with req.session
     // No user info on the session means the user is not logged in
-    if (req.method === 'POST') {
-      const data = JSON.parse(req.body)
-      await db.book.add(data)
-      return res.status(200).end()
-    } else if (req.method === 'DELETE') {
-      const data = JSON.parse(req.body)
-      await db.book.remove(data)
-      return res.status(200).end()
-    } else {
-    return res.status(404).end()
-    }
-  },
+  
+      const user = req.session.user;
+      if (!user) {
+        return res.status(401).end();
+      }
+
+      switch (req.method) {
+        case "POST":
+          try {
+            const info = JSON.parse(req.body)
+            const addBook = await db.book.add(user.id, info)
+            if (!addBook) {
+              req.session.destroy()
+              return res.status(401).end()
+            }
+            return res.status(200).json(addBook)
+          } catch (err) {
+            return res.status(400).json({error: err.message})
+          }
+        case 'DELETE':
+          try {
+            const info = JSON.parse(req.body)
+            const removeBook
+             = await db.book.remove(user.id, info.id)
+            if (!removeBook) {
+              req.session.destroy()
+              return res.status(401).end()
+            }
+            return res.status(200).json(removeBook)
+          } catch (err) {
+            return res.status(400).json({error: err.message})
+          }
+        default:
+          return res.status(404).end()
+      }
+
+
+},
   sessionOptions
 )
